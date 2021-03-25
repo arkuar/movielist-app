bca98007243388a669f5738be711037595171807
arkuar <arttukuronen@gmail.com>
2021-03-25 15:47:05 +0200

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = (env, argv) => {
+  const { mode } = argv
+  return {
+    mode,
+    entry: [
+      './client'
+    ],
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+      path: path.resolve(__dirname, 'build/dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: 'index.html'
+      })
+    ]
+  }
+}
