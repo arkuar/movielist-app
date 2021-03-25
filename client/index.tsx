69749bd65e2c4b7a8cacb3ba8429a8d16a97638b
arkuar <arttukuronen@gmail.com>
2021-03-25 14:39:19 +0200

    Add client folder
    Create separate tsconfig for client and server

---
+++ client/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App: React.FC = () => (
+  <div>
+    Movie list app
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
