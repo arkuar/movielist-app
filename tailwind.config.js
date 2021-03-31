f11af1fc2d49a7c59ea6f2e9c334c142544f01ab
arkuar <arttukuronen@gmail.com>
2021-03-31 13:19:56 +0300

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './build/dist/index.html',
+    './client/**/*.tsx'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,11 @@ module.exports = (env, argv) => {
           test: /\.tsx?$/,
           exclude: /node_modules/,
           loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader', 'postcss-loader']
         }
       ]
     },
