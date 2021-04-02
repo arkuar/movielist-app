5c1927f7b1196e14ad2dfff25d0e196d3f0d9967
arkuar <arttukuronen@gmail.com>
2021-04-02 17:08:28 +0300

    Add id property to Movie type

--- client/MovieListPage/ListItem.tsx
+++ client/MovieListPage/ListItem.tsx
@@ -6,11 +6,11 @@ interface ListProps {
 }
 
 const ListItem: React.FC<ListProps> = ({ movie }) => (
-  <div className="flex w-3/4 rounded-xl border m-3 hover:bg-gray-200">
+  <div className="group cursor-pointer flex w-3/4 rounded-xl border m-3 shadow-sm hover:shadow-inner hover:bg-gray-200">
     <div className="flex-auto p-5">
       <div className="flex flex-wrap">
         <h2 className="flex-auto text-xl font-semibold">{movie.title}</h2>
-        <div className="self-end text-xl font-semibold text-gray-400">{movie.year}</div>
+        <div className="self-end text-xl font-semibold text-gray-500 group-hover:text-black">{movie.year}</div>
       </div>
     </div>
   </div>
--- server/controllers/movieController.ts
+++ server/controllers/movieController.ts
@@ -3,14 +3,17 @@ import { Request, Response } from 'express';
 
 const movies: Movie[] = [
   {
+    id: '0',
     title: 'Godfather',
     year: 1972,
   },
   {
+    id: '1',
     title: 'Dark Knight',
     year: 2008,
   },
   {
+    id: '2',
     title: 'Pulp Fiction',
     year: 1994,
   },
