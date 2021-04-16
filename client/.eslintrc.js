a7a5b021a00a5567f2ae9e988b1d19aa472ab1a1
arkuar <arttukuronen@gmail.com>
2021-04-16 15:25:17 +0300

    Create form for login

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -17,7 +17,10 @@ module.exports = {
   rules: {
     "react/prop-types": "off",
     "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off"
+    "react/react-in-jsx-scope": "off",
+    "jsx-a11y/label-has-associated-control": [ "error", {
+      "controlComponents": ["Field"]
+    }],
   },
   settings: {
     "react": {
--- client/LoginPage/Login.tsx
+++ client/LoginPage/Login.tsx
@@ -1,11 +1,35 @@
+import { Field, Form, Formik } from 'formik';
 import React from 'react';
+
+interface Values {
+  username: string;
+  password: string;
+}
+
+const initialValues: Values = {
+  username: '',
+  password: '',
+};
+
+const onSubmit = async (values: Values) => {
+  console.log(values);
+};
 
 const Login: React.FC = () => (
   <div className="flex flex-col items-center mb-6">
     <div>Login</div>
-    <div>
-      form here
-    </div>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <label htmlFor="username">Username</label>
+        <Field id="username" name="username" placeholder="Username" />
+        <label htmlFor="password">Password</label>
+        <Field id="password" name="password" placeholder="Passowrd" />
+        <button type="submit">Login</button>
+      </Form>
+    </Formik>
   </div>
 );
 
