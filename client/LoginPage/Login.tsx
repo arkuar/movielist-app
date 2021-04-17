06890354a7779e6d3ee26a35aff6ff6ce5bbd0f3
arkuar <arttukuronen@gmail.com>
2021-04-17 14:59:41 +0300

    More refactoring on Login component

--- client/LoginPage/Login.tsx
+++ client/LoginPage/Login.tsx
@@ -8,6 +8,7 @@ import * as yup from 'yup';
 import { useHistory } from 'react-router-dom';
 import loginService from '../util/services/login';
 import LoginFormInput from './LoginFormInput';
+import LoginFormSubmit from './LoginFormSubmit';
 
 const initialValues: LoginValues = {
   username: '',
@@ -50,17 +51,7 @@ const Login: React.FC = () => {
             <LoginFormInput name="username" type="text" label="Username" IconComponent={UserIcon} />
             <LoginFormInput name="password" type="password" label="Password" IconComponent={LockClosedIcon} />
             {!!status && <div className="text-red-500">{status}</div>}
-            <div className="flex w-full pt-4">
-              <button
-                className="bg-blue-500 hover:bg-blue-700 rounded-md flex items-center justify-center w-full transition duration-150 ease-in text-white py-2"
-                type="submit"
-              >
-                <span className="mr-2 uppercase">Login</span>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
-                </svg>
-              </button>
-            </div>
+            <LoginFormSubmit />
           </Form>
         )}
       </Formik>
---
+++ client/LoginPage/LoginFormSubmit.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { LoginIcon } from '@heroicons/react/outline';
+
+const LoginFormSubmit: React.FC = () => (
+  <div className="flex w-full pt-4">
+    <button
+      className="bg-blue-500 hover:bg-blue-700 rounded-md flex items-center justify-center w-full transition duration-150 ease-in text-white py-2"
+      type="submit"
+    >
+      <span className="mr-2 uppercase">Login</span>
+      <LoginIcon className="h-6 w-6" />
+    </button>
+  </div>
+);
+
+export default LoginFormSubmit;
