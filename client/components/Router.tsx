8ce13945dd33c8433b43a70a1b524a8b2c4c35ef
arkuar <arttukuronen@gmail.com>
2021-04-26 16:51:31 +0300

    Add redirects when user is logged in

--- client/components/Router.tsx
+++ client/components/Router.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import MovieList from '../MovieListPage/MovieList';
 import Movie from '../MovieListPage/Movie';
 import Login from '../LoginPage/Login';
 import SignUp from '../SignUpPage/SignUp';
+import useAuth from '../util/hooks/useAuth';
 
-const Router: React.FC = () => (
-  <Switch>
-    <Route exact path="/">
-      <MovieList />
-    </Route>
-    <Route path="/movies/:id">
-      <Movie />
-    </Route>
-    <Route path="/login">
-      <Login />
-    </Route>
-    <Route path="/signup">
-      <SignUp />
-    </Route>
-  </Switch>
-);
+const Router: React.FC = () => {
+  const [{ username }] = useAuth();
+
+  return (
+    <Switch>
+      <Route exact path="/">
+        <MovieList />
+      </Route>
+      <Route path="/movies/:id">
+        <Movie />
+      </Route>
+      <Route path="/login">
+        {username
+          ? <Redirect to="/" />
+          : <Login />}
+      </Route>
+      <Route path="/signup">
+        {username
+          ? <Redirect to="/" />
+          : <SignUp />}
+      </Route>
+    </Switch>
+  );
+};
 
 export default Router;
