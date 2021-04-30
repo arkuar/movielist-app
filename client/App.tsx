b54b9e326ba9b52bd57a2db1bf043f3093e08f6b
arkuar <arttukuronen@gmail.com>
2021-04-30 12:52:17 +0300

    Install react-toastify for notifications

--- client/App.tsx
+++ client/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import NavBar from './components/NavBar';
 import Router from './components/Router';
 import { AuthProvider, authReducer } from './util/contexts/index';
@@ -8,6 +9,7 @@ const App: React.FC = () => (
   <AuthProvider reducer={authReducer}>
     <BrowserRouter>
       <NavBar />
+      <ToastContainer />
       <Router />
     </BrowserRouter>
   </AuthProvider>
