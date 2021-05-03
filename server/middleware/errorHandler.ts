636c9243a0e5d0487c7ff59889483771c450130b
arkuar <arttukuronen@gmail.com>
2021-05-03 15:14:01 +0300

    Refactor errorHandler

--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -1,18 +1,28 @@
 import { NextFunction, Request, Response } from 'express';
+import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
+import { Error } from 'mongoose';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const errorHandler = (error: any, _req: Request, res: Response, next: NextFunction) => {
-  if (error.name === 'ValidationError') {
-    if (error.errors.username) {
-      const { message, path } = error.errors.username;
+export const errorHandler = (err: any, _req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error.ValidationError) {
+    if (err.errors.username) {
+      const { message, path } = err.errors.username;
       return res.status(400).json({ error: message, field: path });
     }
-    return res.status(400).json({ error: error.message });
+    return res.status(400).json({ error: err.message });
   }
 
-  if (error.name === 'CastError') {
+  if (err instanceof Error.CastError) {
     return res.status(400).send({ error: 'Not a valid ID' });
   }
 
-  return next(error);
+  if (err instanceof JsonWebTokenError) {
+    return res.status(401).send({ error: 'Invalid token' });
+  }
+
+  if (err instanceof TokenExpiredError) {
+    return res.status(401).send({ error: 'Token expired' });
+  }
+
+  return next(err);
 };
