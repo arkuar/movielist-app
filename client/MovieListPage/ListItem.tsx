b56e91c9ae87fbdde98d2d5dd41535c6aac997fa
arkuar <arttukuronen@gmail.com>
2021-07-08 16:12:02 +0300

    Add poster to list item

--- client/MovieListPage/ListItem.tsx
+++ client/MovieListPage/ListItem.tsx
@@ -9,12 +9,17 @@ interface ListProps {
 const ListItem: React.FC<ListProps> = ({ movie }) => (
   <Link
     to={`/movies/${movie.id}`}
-    className="group cursor-pointer flex w-3/4 rounded-xl border m-3 shadow-sm hover:shadow-inner hover:bg-gray-200"
+    className="group cursor-pointer flex w-3/4 rounded-xl max-h-32 border m-3 shadow-sm hover:shadow-inner hover:bg-gray-200"
   >
-    <div className="flex-auto p-5">
-      <div className="flex flex-wrap">
+    {movie.poster && (
+      <div className="flex flex-none w-20">
+        <img src={movie.poster} alt="Movie poster" className="rounded object-cover" />
+      </div>
+    )}
+    <div className="flex-1 p-5 self-center">
+      <div className="flex flex-col md:flex-row">
         <h2 className="flex-auto text-xl font-semibold">{movie.title}</h2>
-        <div className="self-end text-xl font-semibold text-gray-500 group-hover:text-black">{movie.year}</div>
+        <div className="md:self-end text-xl font-semibold text-gray-500 group-hover:text-black">{movie.year}</div>
       </div>
     </div>
   </Link>
